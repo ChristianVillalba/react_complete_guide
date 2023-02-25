23a979eed64785262d61bc63ec5bd85e9dc10dfe
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-02-25 22:09:13 +0000

    changes hard coded date for injected data using JS

--- 01-starting-project/src/components/ExpenseItem.js
+++ 01-starting-project/src/components/ExpenseItem.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2023 , 2 , 25);
+  const expenseTitle = "Beers";
+  const expenseAmount = 99.99;
+
+
   return (
     <div className="expense-item">
       <div>May 15th 2023</div>
       <div className="expense-item__description">
-        <h2>Book</h2>
-        <div className="expense-item__price">£ 9.99</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">£ {expenseAmount}</div>
       </div>
     </div>
   );
