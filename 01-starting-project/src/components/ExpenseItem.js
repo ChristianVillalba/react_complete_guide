0795b2c243a2cc8d44baf2d6692f59dc3ad02c5b
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-02-27 21:39:00 +0000

    adds props

--- 01-starting-project/src/components/ExpenseItem.js
+++ 01-starting-project/src/components/ExpenseItem.js
@@ -1,19 +1,14 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-
-  const expenseDate = new Date(2023 , 2 , 25);
-  const expenseTitle = "Beers";
-  const expenseAmount = 99.99;
-
+function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>May 15th 2023</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">£ {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£ {props.amount}</div>
       </div>
     </div>
   );
